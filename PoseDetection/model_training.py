0994629b732959de48a27048f96219e9b176a3bb
Francis Chen <200520598@student.georgianc.on.ca>
2025-05-15 09:22:15 -0400

    。

--- PoseDetection/model_training.py
+++ PoseDetection/model_training.py
@@ -24,18 +24,18 @@ class Trainer:
         # Set up models with their respective window sizes
         self.models = [
             CNNModel(),
-            # TCNModel(),
-            # CRNNModel(),
-            # LSTMAttentionModel(),
-            # ResNET1DModel(),
-            # EfficientNet1DModel(),
-            # InceptionTimeModel(),
-            # TransformerLiteModel(),
-            # TFTLiteModel(),
-            # SEResNET1DModel(),
-            # WaveNetModel(),
-            # TCNSEModel(),
-            # ResNET1DTcnHybridModel(),
+            TCNModel(),
+            CRNNModel(),
+            LSTMAttentionModel(),
+            ResNET1DModel(),
+            EfficientNet1DModel(),
+            InceptionTimeModel(),
+            TransformerLiteModel(),
+            TFTLiteModel(),
+            SEResNET1DModel(),
+            WaveNetModel(),
+            TCNSEModel(),
+            ResNET1DTcnHybridModel(),
         ]
 
     def train(self):
